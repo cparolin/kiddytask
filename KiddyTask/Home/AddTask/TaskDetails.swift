1b9d479214acf1127ccd73dfe7f7db26a22f25bc
Camila Parolin <cparolin@Parolins-MacBook-Pro.local>
2025-07-24 17:33:59 -0300

    commit dia 9

--- KiddyTask/Home/AddTask/TaskDetails.swift
+++ KiddyTask/Home/AddTask/TaskDetails.swift
@@ -13,7 +13,70 @@ struct TaskDetails: View {
     @State var task: KidTask
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack{
+                HStack{
+                    VStack{
+                        Text("Picture Before 📷")
+                        
+                        Image(uiImage: ((UIImage(data: task.imageBefore ?? Data()) ?? UIImage(named: "photoItem")) ?? UIImage()))
+                            .resizable()
+                            .frame(width: 175, height: 175)
+                            .cornerRadius(24)
+                            
+                    }
+                    .padding()
+                    
+                    VStack{
+                        HStack{
+                            Text("Why it's important to do this task?")
+                                .frame(width: 300, height: 100)
+                            Image("guardianDuvida")
+                        }
+//                        .padding()
+                        
+                        Text(task.why ?? "noAnswer")
+                            .frame(width: 300, height: 100)
+                            .border(.red)
+                        
+                        Text("Task Description 🖍️")
+                            .frame(width: 200, height: 100)
+                        
+                        Text(task.desc ?? "noDescriptionposs")
+                            .frame(width: 300, height: 100)
+                            .border(.red)
+                    }
+                    .padding(.leading, 235)
+                }
+                .padding(.bottom, 100)
+                
+                
+                HStack{
+                    ZStack{
+                        Rectangle()
+                            .frame(width: 370, height: 330)
+                            .foregroundStyle(Color("FrontYellow"))
+                        VStack{
+                            Text("Date to do the task 📅")
+                        }
+                    }
+                    .padding(.trailing, 30)
+                    
+                    ZStack{
+                        Rectangle()
+                            .frame(width: 370, height: 330)
+                            .foregroundStyle(Color("FrontYellow"))
+                        
+                        VStack{
+                            Text("Time to do this task ⏰")
+                        }
+                    }
+                }
+                
+            }
+            
+            .navigationTitle("name")
+        }
     }
 }
 
