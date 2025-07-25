1c6b11713195040cde0658e96c7863864e26e790
Camila Parolin <cparolin@Parolins-MacBook-Pro.local>
2025-07-25 17:51:14 -0300

    commit dia 10

---
+++ KiddyTask/Guardian/GuardianView.swift
@@ -0,0 +1,18 @@
+//
+//  GuardianView.swift
+//  KiddyTask
+//
+//  Created by Camila Parolin on 25/07/25.
+//
+
+import SwiftUI
+
+struct GuardianView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GuardianView()
+}
